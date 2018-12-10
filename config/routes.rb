e584be8ad35d6e6b172217a964d87a2222283522
risa.obayashi <r.merino.xxx@gmail.com>
2018-12-10 15:17:28 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
   root to: 'tasks#index'
   resources :tasks
 end
